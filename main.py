1b5167bc956fc9d4e2315547fbaad0d884880317
Nurlan-Aliev <nualiev612@gmail.com>
2025-11-20 20:00:06 +0400

    style

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from classes.hero import Gryffindor, Slytherin, Ravenclaw, Hufflepuff
+from config import config
 from launcher import navigate_rooms
 from map import corridor
 from utils import input_data, join
@@ -22,16 +23,7 @@ def beginning(name):
 
 
 def play_game():
-    print(
-        "Профессор Вуд дает задание приготовить зелье лунного света. Нам нужно подготовиться к следующему уроку, до которого",
-        "осталось совсем немного времени. Правда, я полностью про это забыл, и теперь времени у меня почти не осталось.",
-        "Ко всему прочему, профессор ничего не говорит о том, где искать ингредиенты, как готовить зелье и даже из чего оно состоит.",
-        "Он предлагает нам самим найти рецепт и приготовить это зелье. Что ж, мне кажется, лучший способ начать — отправиться в библиотеку.",
-        "Возможно, в книгах я найду какую-нибудь подсказку, которая поможет справиться с этим заданием.",
-        "Главное — успеть собрать все ингредиенты и прийти в кабинет профессора Вуда.\n",
-        sep="\n",
-    )
-
+    print(config.scenario.start)
     name = input_data("By the way what is your name my friend?")
     hero = beginning(name)
     navigate_rooms(hero, corridor)
